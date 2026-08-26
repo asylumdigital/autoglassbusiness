1e3ddc551bd8cf8b9dd4d04ba022ad770b0f3bef
Phil Baker <phil@philsbury.co.uk>
2026-08-26 17:09:31 +0100

    Format

--- app/Config/widgets.php
+++ app/Config/widgets.php
@@ -2,22 +2,21 @@
 
 return [
   'widgets' => [
-    [
-        'name'          => 'Static items',
-        'id'            => 'sidebar',
-        'before_widget' => '<div id="%1$s" class="pt-20 widget %2$s">',
-        'after_widget'  => '</div>',
-        'before_title'  => '<h4 class="widget-title">',
-        'after_title'   => '</h4>'
-    ],
-    [
-        'name'          => 'Sticky items',
-        'id'            => 'sticky-sidebar',
-        'before_widget' => '<div id="%1$s" class="sticky top-22 hidden md:block widget %2$s">',
-        'after_widget'  => '</div>',
-        'before_title'  => '<h4 class="widget-title">',
-        'after_title'   => '</h4>'
-    ],
-
+        [
+            'name'          => 'Static items',
+            'id'            => 'sidebar',
+            'before_widget' => '<div id="%1$s" class="pt-20 widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h4 class="widget-title">',
+            'after_title'   => '</h4>'
+        ],
+        [
+            'name'          => 'Sticky items',
+            'id'            => 'sticky-sidebar',
+            'before_widget' => '<div id="%1$s" class="sticky top-22 hidden md:block widget %2$s">',
+            'after_widget'  => '</div>',
+            'before_title'  => '<h4 class="widget-title">',
+            'after_title'   => '</h4>'
+        ],
     ]
 ];
