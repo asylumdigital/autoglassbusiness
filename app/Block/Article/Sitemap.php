967ede06c7e4e50e87a58565bcd384c9cf721e31
Phil Baker <phil@philsbury.co.uk>
2026-08-28 19:05:48 +0100

    Added fields for sitemap

--- app/Block/Article/Sitemap.php
+++ app/Block/Article/Sitemap.php
@@ -3,6 +3,7 @@
 namespace Asylum\Theme\Block\Article;
 
 use Asylum\Block\BlockController;
+use StoutLogic\AcfBuilder\FieldsBuilder;
 use Timber\PostQuery;
 use Timber\Timber;
 
@@ -16,17 +17,48 @@ class Sitemap extends BlockController
         'template-policy.twig',
     ];
 
+    protected function fields(FieldsBuilder $fields): FieldsBuilder
+    {
+        $postTypes = get_post_types(['public' => true], 'objects');
+
+        unset($postTypes['attachment']);
+
+        $postTypes = array_map(fn($type) => $type->label, $postTypes);
+
+        $fields
+            ->addRepeater('sections')
+                ->addText('title')
+                    ->setWidth(50)
+                ->addSelect('post_type', [
+                    'choices' => $postTypes
+                ])
+                    ->setWidth(50)
+                ;
+        return $fields;
+    }
+
     protected function transform(&$data, array $args = []): void
     {
-        $data['sitemap']['pages'] = [
-            'title' => '',
-            'items' => $this->getLinks('page'),
-        ];
+        if (empty($data['sections'])) {
+            $data['sitemap']['pages'] = [
+                'title' => 'Pages',
+                'items' => $this->getLinks('page'),
+            ];
 
-        $data['sitemap']['posts'] = [
-            'title' => 'Resource centre',
-            'items' => $this->getLinks('post'),
-        ];
+            $data['sitemap']['posts'] = [
+                'title' => 'Resource centre',
+                'items' => $this->getLinks('post'),
+            ];
+
+            return;
+        }
+
+        foreach ($data['sections'] as $section) {
+            $data['sitemap'][$section['post_type']] = [
+                'title' => $section['title'],
+                'items' => $this->getLinks($section['post_type']),
+            ];
+        }
     }
 
     private function getLinks($postType = 'post'): PostQuery
