2a30e2741da697dc4e3a715b042391ad009ae5d0
Phil Baker <phil@philsbury.co.uk>
2026-09-09 21:54:09 +0100

    UPdated hero

--- app/Block/Hero/Video.php
+++ app/Block/Hero/Video.php
@@ -3,12 +3,13 @@
 namespace Asylum\Theme\Block\Hero;
 
 use Asylum\Block\BlockController;
+use StoutLogic\AcfBuilder\FieldsBuilder;
 
 class Video extends BlockController
 {
     protected ?string $name = 'hero-video';
 
-    protected ?string $label = 'TBC HERO';
+    protected ?string $label = 'Image hero';
 
     protected string $category = 'asylum-hero';
 
@@ -19,4 +20,42 @@ class Video extends BlockController
     protected array $disallowedTemplates = [
         'template-policy.twig',
     ];
+
+    protected function fields(FieldsBuilder $fields): FieldsBuilder
+    {
+        $fields
+            ->addText('eyebrow', [
+                'label' => 'Accent title',
+            ])
+            ->addTextarea('title')
+            ->addTextarea('content')
+            ->addGroup('buttons')
+                ->addLink('primary')
+                ->addLink('secondary')
+                ->endGroup()
+            ->addImage('mobile')
+            ->addImage('big');
+        return $fields;
+    }
+
+    protected function transform(&$data, array $args = []): void
+    {
+        add_action('wp_head', function() use ($data) {
+            $small = $data['mobile']['url'];
+            $large = $data['big']['url'];
+            echo <<<HTML
+                <style>
+                    :root {
+                        --header-bg: url($small);
+                    }
+
+                    @media (width >= 1024px) {
+                        :root {
+                            --header-bg: url($large);
+                        }
+                    }
+                </style>
+            HTML;
+        });
+    }
 }
