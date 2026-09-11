f069c1e0e4a616f9a6bd3bb64e4a1dc4006e5e5c
Phil Baker <phil@philsbury.co.uk>
2026-09-11 17:50:16 +0100

    Fixed menu open/close

--- app/Resources/assets/js/public/main.js
+++ app/Resources/assets/js/public/main.js
@@ -3,14 +3,14 @@ import Alpine from "alpinejs";
 import { ScrollSpy } from './components/jump-links';
 import { Carousel } from "./components/carousel/content";
 
-import { YouTube, VimeoPlayer, Local } from "./components/video";
+import { YouTube, Local } from "./components/video";
 
 Alpine.data('jump_links', ScrollSpy);
 Alpine.data('carousel', Carousel);
 
 Alpine.data('youtube', YouTube);
 Alpine.data('direct', Local);
-Alpine.data('vimeo', VimeoPlayer);
+// Alpine.data('vimeo', VimeoPlayer);
 
 Alpine.start();
 
