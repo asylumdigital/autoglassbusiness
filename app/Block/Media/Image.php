f704013d8d14c12e300b82cb628b9b22f4afcd00
Phil Baker <phil@philsbury.co.uk>
2026-09-13 12:12:04 +0100

    Image adjustments

---
+++ app/Block/Media/Image.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Asylum\Theme\Block\Media;
+
+use Asylum\Block\BlockController;
+use StoutLogic\AcfBuilder\FieldsBuilder;
+
+class Image extends BlockController
+{
+    protected ?string $name = 'image';
+
+    protected ?string $label = 'Image';
+
+    protected string $category = 'asylum-media';
+
+    protected string $icon = 'format-image';
+
+    protected function fields(FieldsBuilder $fields): FieldsBuilder
+    {
+        $fields
+            ->addImage('image', [
+                'preview_size' => 'thumbnail',
+            ])
+            ->addText('image_caption', [
+                'label' => 'Caption'
+            ])
+                ->setWidth(50)
+            ->addText('image_alt', [
+                'label' => 'Alternative text',
+            ])
+                ->setWidth(50)
+            ->addSelect('aspect', [
+                'choices' => [
+                    'square' => 'Square',
+                    'video' => 'Video',
+                    'natural' => 'Natural'
+                ],
+                'default_value' => 'video',
+            ])
+            ->addTrueFalse('max_width', [
+                'ui' => true,
+            ]);
+        return $fields;
+    }
+
+    public function declaration(): array
+    {
+        return [
+            'parent' => [ "acf/prose" ],
+        ];
+    }
+}
